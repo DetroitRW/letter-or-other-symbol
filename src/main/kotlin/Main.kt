c15db3e30fc6edbc8235512b0d548caee4c04158
Ilya Brant <detroitib31@gmail.com>
2022-11-18 21:02:12 +0600

    Доделал задачу

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,30 @@
 import java.util.Scanner
 
 fun main(args: Array<String>) {
-
     val reader = Scanner(System.`in`)
 
     print("Введите код символа: ")
-    val n = reader.nextInt()
+    val n = Char(reader.nextInt())
 
-    val a = "a".toInt()
-    val z = "z".toInt()
-    val A = "A".toInt()
-    val Z = "Z".toInt()
-
-
-    if (a <= n <= z || A <= n <= Z) {
-    print("Это буква", Char(n))
+    if ((n in 'a'..'z') or (n in 'A'..'Z')) {
+        print("Это буква: $n")
+    } else {
+        print("Это не буква, а символ: $n")
     }
-    else {
-    print("Это не буква, а символ", Char(n))
-    }
+//    val reader = Scanner(System.`in`)
+//
+//    print("Введите код символа: ")
+//    val n = reader.nextInt()
+//
+//    val a = 'a'.code
+//    val z = 'z'.code
+//    val A = 'A'.code
+//    val Z = 'Z'.code
+//
+//    if ((n in a..z) or (n in A..Z)) {
+//        print("Это буква: ${Char(n)}")
+//    } else {
+//        print("Это не буква, а символ: ${Char(n)}")
+//    }
 }
 
